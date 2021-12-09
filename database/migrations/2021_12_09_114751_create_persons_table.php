c16694ef72acd94a517c674c5c57e9067f4b2f33
dmitry <dimaorizz@gmail.com>
2021-12-09 14:06:12 +0200

    created migration

--- database/migrations/2021_12_09_114751_create_persons_table.php
+++ database/migrations/2021_12_09_114751_create_persons_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePasswordResetsTable extends Migration
+class CreatePersonsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,12 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('persons', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("first_name")->nullable();
+            $table->string("middle_name")->nullable();
+            $table->string("last_name")->nullable();
         });
     }
 
@@ -27,6 +29,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('persons');
     }
 }
